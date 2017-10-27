42ddc1c437aa2f98c625ec214401ac391a7a8aa3
psychonaut04 <diogotimote@gmail.com>
2017-10-27 19:25:58 -0200

    Fix addons bug
    Now it get the current player addon too to change color

--- data/creaturescripts/scripts/teams.lua
+++ data/creaturescripts/scripts/teams.lua
@@ -1,7 +1,7 @@
 --One guild need to have the id 1, here is the red team/guild
 function onLogin(player)
-    local redlooktype = {lookType = player:getOutfit().lookType, lookHead = 74, lookBody = 94, lookLegs = 113, lookFeet = 132}
-    local bluelooktype = {lookType = player:getOutfit().lookType, lookHead = 49, lookBody = 86, lookLegs = 69, lookFeet = 105}
+    local redlooktype = {lookAddons =  player:getOutfit().lookAddons, lookType = player:getOutfit().lookType, lookHead = 74, lookBody = 94, lookLegs = 113, lookFeet = 132}
+    local bluelooktype = { lookAddons =  player:getOutfit().lookAddons, lookType = player:getOutfit().lookType, lookHead = 49, lookBody = 86, lookLegs = 69, lookFeet = 105}
 
 	local guild = player:getGuild():getId()
 
